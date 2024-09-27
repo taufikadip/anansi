ae4e1d38d7a43c1510b577864c52ac4be68ad086
taufikadip <77040564+taufikadip@users.noreply.github.com>
2024-09-27 14:31:01 +0700

    Add exercise kotlin

--- exercise/day-1/HelloWorld/src/Main.kt
+++ exercise/day-1/HelloWorld/src/Main.kt
@@ -1,15 +1,49 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    var world = "World"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + world + "!")
+//    val name = "Kotlin"
+//    var world = "World"
+//    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+//    // to see how IntelliJ IDEA suggests fixing it.
+//    println("Hello, " + world + "!")
+//
+//    for (i in 1..5) {
+//        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+//        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+//        println("i = $i")
+//    }
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    val day = 12
+
+    val result = when (day) {
+        1 -> "Monday"
+        2 -> "Tuesday"
+        3 -> "Wednesday"
+        4 -> "Thursday"
+        5 -> "Friday"
+        6 -> "Saturday"
+        7 -> "Sunday"
+        else -> "Invalid day."
     }
+    println(result) // Outputs "Thursday" (day 4)
+
+    var name = "John"      // String (text)
+    val birthyear = 1975   // Int (number)
+
+    println(name)          // Print the value of name
+    println(birthyear)     // Print the value of birthyear
+
+    var x = 10
+    x += 5
+
+    println(x)
+
+    var txt = "Hello World"
+    println(txt[0]) // first element (H)
+    println(txt[2]) // third element (l)
+
+    println('string')
+    List<User> user = new ArrayList<>()
+    user.add
+
 }
